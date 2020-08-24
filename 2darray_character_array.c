ece96bd558a6d50fb7b309963342b68c4a196979
One-Exploit's <moarquam997320@gmail.com>
2020-08-24 04:57:41 -0700

    this is array of pointers

---
+++ 2darray_character_array.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdlib.h>
+int main(){
+    char *x[10]={"hihoware","f","g","WRr","werfwe","EQeq","frd","ef","8888","Rgf"};
+      printf("%p\n",&x);
+      printf("%p",x);
+
+      
+     
+    return 0;
+   }
