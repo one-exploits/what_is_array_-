59307cbeed81d4fd56374b5ac8ca7f4adcf7b7b7
One-Exploit's <oneexploits@gmail.com>
2020-08-24 17:29:20 +0530

    Update 2darray_character_array.c

--- 2darray_character_array.c
+++ 2darray_character_array.c
@@ -5,6 +5,10 @@ int main(){
     char *x[10]={"hihoware","f","g","WRr","werfwe","EQeq","frd","ef","8888","Rgf"};
       printf("%p\n",&x);
       printf("%p",x);
+    //output are the same 
+      //x>>have address of x[0] varaible
+      //okay
+
 
       
      
